b65f52540fe13dc9298eda9c662f55196e218a3a
BuildTools <unconfigured@null.spigotmc.org>
2024-07-22 01:22:35 -0700

    ChipOptimizer Virtual+Fusion VirtualChip and ChipFusion are combined. No exceptions on run, but the fusions are still incorrect.

--- NieR.Automata.Editor/ChipOptimizer.cs
+++ NieR.Automata.Editor/ChipOptimizer.cs
@@ -88,6 +88,13 @@ namespace NieR.Automata.Toolkit
                 return null;
             }
 
+            public int GetCountOf(ChipCode code)
+            {
+                if (_set.TryGetValue(code, out var chips))
+                    return chips.Count;
+                return 0;
+            }
+
             public IEnumerator<VirtualChip> GetEnumerator()
             {
                 return new ChipSet_Enumerator(this);
@@ -195,130 +202,262 @@ namespace NieR.Automata.Toolkit
             }
         }
 
-        // Simple struct to associate two chips prior to a fusion
-        public class ChipFusion
-        {
-            public ChipCode Code;
-            public VirtualChip Lower;
-            public VirtualChip Upper;
-
-            private ChipFusion() { Lower = null; Upper = null; }
-            private ChipFusion(VirtualChip lower, VirtualChip upper)
-            {
-                Lower = lower;
-                Upper = upper;
-            }
-
-            public bool IsComplete()
-            {
-                if (Lower == null) return false;
-                if (!(Lower.Fusion?.IsComplete() ?? true)) return false;
-
-                if (Upper == null) return false;
-                if (!(Upper.Fusion?.IsComplete() ?? true)) return false;
-
-                return true;
-            }
-
-            public bool IsPartial() { return Lower != null ^ Upper != null; }
-            public bool IsEmpty() { return Lower == null && Upper == null; }
-
-            public static ChipFusion Make(ChipCode code, ref ChipSet set)
-            {
-                // Can't make a fusion for a level 0
-                if (code.Level <= 0) return null;
-
-                ChipFusion fusion = new ChipFusion();
-                fusion.Code = code;
-
-                ChipCode highestLower = code.GetDefuseLower();
-                fusion.Lower = VirtualChip.AcquireLowest(highestLower, ref set);
-
-                ChipCode complement = fusion.Lower?.Code.GetFuseComplement(code) ?? highestLower.GetFuseComplement(code);
-                fusion.Upper = VirtualChip.AcquireExact(complement, ref set);
-
-                return fusion.IsEmpty() ? null : fusion;
-            }
-
-            public void GreedyPass(ref ChipSet set, ref List<ChipFusion> fusions)
-            {
-                if (IsEmpty()) return; // This is an error state, but it doesn't matter too much
-
-                if (Lower == null)
-                    Lower = VirtualChip.AcquireHighest(Upper.Code.GetFuseComplement(Code), ref set);
-                if (Upper == null)
-                    Upper = VirtualChip.AcquireHighest(Lower.Code.GetFuseComplement(Code), ref set);
-                
-                Lower?.GreedyPass(ref set, ref fusions);
-                Upper?.GreedyPass(ref set, ref fusions);
-                
-                if (IsComplete()) fusions.Add(this);
-            }
-        }
-
         // A "virtual" chip; could be an actual chip or a result of a pending fusion
         public class VirtualChip
         {
+            // Code containing the stats this chip represents
             public ChipCode Code { get; private set; }
-            public Chip Actual { get; private set; } = null;
-            public ChipFusion Fusion { get; private set; } = null;
 
+            // A reference to an actual chip this virtual chip wraps. If null, this chip is a fusion
+            public Chip Actual { get; private set; } = null;
+
+            // The chip with a lower weight value if this chip is a fusion
+            public VirtualChip Lower { get; private set; } = null;
+
+            // The chip with a higher weight value if this chip is a fusion
+            public VirtualChip Upper { get; private set; } = null;
+
+
+            // Name of the chip this virtual chip represents
             public String Name => Chip.Chips[Code.Type].Name;
+
+            // Level of the chip this virtual chip represents
             public int Level => Code.Level;
+
+            // Weight of the chip this virtual chip represents
             public int Weight => Code.Weight;
 
+            
+            // True if this chip does not directly contain an actual chip
+            public bool IsFusion => Actual == null;
+
+            // True if there is no actual chips associated with this or its children
+            public bool IsEmpty => IsFusion && (Lower?.IsEmpty ?? true) && (Upper?.IsEmpty ?? true);
+            
+            // True if this chip either is a chip or can fuse into a chip
+            public bool IsFullChip => (!IsFusion) || CanFuse;
+
+            // True if this chip is a fusion and has children it can fuse together
+            public bool CanFuse => IsFusion && (Lower?.IsFullChip ?? false) && (Upper?.IsFullChip ?? false);
+
+
+            // Constructs this chip in order to wrap an actual chip
             public VirtualChip(Chip chip) { Code = new ChipCode(chip); Actual = chip; }
+            
+            // Constructs this chip to represent a fusion of some kind
             private VirtualChip(ChipCode code) { Code = code; }
 
-            public bool IsValid() { return Actual == null ^ Fusion == null; }
-            public bool IsFusion() { return Actual == null; }
 
-            public static VirtualChip AcquireExact(in ChipCode code, ref ChipSet set)
+            /// <summary>
+            /// Either claims actual chips or creates chip fusions for the requested chip code and qty.
+            /// This will only claim chips of the exact weight requested.
+            /// </summary>
+            /// <param name="set">The ChipSet to try and claim chips from</param>
+            /// <param name="code">Code representing the desired chip</param>
+            /// <param name="qty">How many chips to acquire</param>
+            /// <param name="fuse">If true, will create fusions to make up non-found chips</param>
+            /// <returns>
+            /// A list of chips. 
+            ///   If fuse=true,  this will be qty long.
+            ///   If fuse=false, this will be up to qty long and will only contain found chips.
+            /// </returns>
+            public static List<VirtualChip> AcquireExact(ref ChipSet set, in ChipCode code, int qty = 1, bool fuse = true)
             {
-                // Try and get an existing chip
-                VirtualChip chip = set.Pop(code);
-                if (chip != null) return chip;
-
-                // Otherwise, create a new chip based on a fusion
-                chip = new VirtualChip(code);
-                chip.Fusion = ChipFusion.Make(code, ref set);
-                return chip.Fusion == null ? null : chip;
-            }
-
-            public static VirtualChip AcquireLowest(in ChipCode code, ref ChipSet set)
-            {
-                // Try and get an existing chip
-                VirtualChip chip;
-                for (int i = Chip.MinimumWeightForLevel[code.Level]; i <= code.Weight; i++)
+                List<VirtualChip> output = new List<VirtualChip>();
+                
+                // Try and get existing chips
+                while (output.Count < qty)
                 {
-                    chip = set.Pop(new ChipCode(code.Type, code.Level, i));
-                    if (chip != null) return chip;
+                    var chip = set.Pop(code);
+                    if (chip != null) output.Add(chip);
+                    else break;
                 }
 
                 // Otherwise, create a new chip based on a fusion
-                chip = new VirtualChip(code);
-                chip.Fusion = ChipFusion.Make(code, ref set);
-                return chip.Fusion == null ? null : chip;
+                if ((output.Count != qty) && fuse)
+                    output.AddRange(MakeFusion(ref set, code, qty - output.Count));
+                return output;
             }
 
-            public static VirtualChip AcquireHighest(in ChipCode code, ref ChipSet set)
+            /// <summary>
+            /// Either claims actual chips or creates chip fusions for the requested chip code and qty.
+            /// This will claim chips equal to or below the weight requested, with a preference for low weight.
+            /// </summary>
+            /// <param name="set">The ChipSet to try and claim chips from</param>
+            /// <param name="code">Code representing the desired chip</param>
+            /// <param name="qty">How many chips to acquire</param>
+            /// <param name="fuse">If true, will create fusions to make up non-found chips</param>
+            /// <returns>
+            /// A list of chips. 
+            ///   If fuse=true,  this will be qty long.
+            ///   If fuse=false, this will be up to qty long and will only contain found chips.
+            /// </returns>
+            public static List<VirtualChip> AcquireLowest(ref ChipSet set, in ChipCode code, int qty = 1, bool fuse = true)
             {
-                // Try and get an existing chip
-                VirtualChip chip;
+                List<VirtualChip> output = new List<VirtualChip>();
+
+                // Try and get existing chips
+                for (int i = Chip.MinimumWeightForLevel[code.Level]; i >= code.Weight; i--)
+                {
+                    while (output.Count < qty)
+                    {
+                        var chip = set.Pop(new ChipCode(code.Type, code.Level, i));
+                        if (chip != null) output.Add(chip);
+                        else break;
+                    }
+                }
+
+                // Otherwise, create a new chip based on a fusion
+                if ((output.Count != qty) && fuse)
+                    output.AddRange(MakeFusion(ref set, code, qty - output.Count));
+                return output;
+            }
+
+            /// <summary>
+            /// Either claims actual chips or creates chip fusions for the requested chip code and qty.
+            /// This will claim chips equal to or below the weight requested, with a preference for high weight.
+            /// </summary>
+            /// <param name="set">The ChipSet to try and claim chips from</param>
+            /// <param name="code">Code representing the desired chip</param>
+            /// <param name="qty">How many chips to acquire</param>
+            /// <param name="fuse">If true, will create fusions to make up non-found chips</param>
+            /// <returns>
+            /// A list of chips. 
+            ///   If fuse=true,  this will be qty long.
+            ///   If fuse=false, this will be up to qty long and will only contain found chips.
+            /// </returns>
+            public static List<VirtualChip> AcquireHighest(ref ChipSet set, in ChipCode code, int qty = 1, bool fuse = true)
+            {
+                List<VirtualChip> output = new List<VirtualChip>();
+
+                // Try and get existing chips
                 for (int i = code.Weight; i >= Chip.MinimumWeightForLevel[code.Level]; i--)
                 {
-                    chip = set.Pop(new ChipCode(code.Type, code.Level, i));
-                    if (chip != null) return chip;
+                    while (output.Count < qty)
+                    {
+                        var chip = set.Pop(new ChipCode(code.Type, code.Level, i));
+                        if (chip != null) output.Add(chip);
+                        else break;
+                    }
                 }
 
-                // In this particular variant, we're assuming there is no viable fusion
-                return null;
+                // Otherwise, create a new chip based on a fusion
+                if ((output.Count != qty) && fuse)
+                    output.AddRange(MakeFusion(ref set, code, qty - output.Count));
+                return output;
             }
 
-            public void GreedyPass(ref ChipSet set, ref List<ChipFusion> fusions)
+            /// <summary>
+            /// Creates fusions for the request chip code and qty.
+            /// </summary>
+            /// <param name="set">The ChipSet to try and claim chips from</param>
+            /// <param name="code">Code representing the desired chip</param>
+            /// <param name="qty">How many chips to create fusions for</param>
+            /// <returns>A list of fusions. This will be qty long</returns>
+            public static List<VirtualChip> MakeFusion(ref ChipSet set, in ChipCode code, int qty = 1)
             {
-                if (!IsFusion()) return;
-                Fusion.GreedyPass(ref set, ref fusions);
+                // This function guarantees delivery of items. Therefore, prep the whole list in advance
+                List<VirtualChip> output = new List<VirtualChip>();
+                for (int i = 0; i < qty; i++) output.Add(new VirtualChip(code));
+
+                // Can't make fusions for level 0 chips
+                if (code.Level <= 0) return output;
+
+                // Even and odd chip levels are processed different to help assert delivery
+                if (code.Level % 2 == 1)
+                {
+                    // Obtain the lower chips needed for fusion
+                    ChipCode highestLower = code.GetDefuseLower();
+                    {
+                        List<VirtualChip> lowers = AcquireLowest(ref set, highestLower, qty);
+                        lowers.Sort((x, y) => x.Code > y.Code);
+                        for (int i = 0; i < qty; i++) output[i].Lower = lowers[i];
+                    }
+
+                    // Obtain the upper chips; since the lower chips may be different levels,
+                    //  we count out how many are of a certain level and batch the uppers
+                    int lastProcessed = 0;
+                    while (lastProcessed < qty)
+                    {
+                        // This section gets a count of identical chips
+                        ChipCode common = output[lastProcessed].Lower?.Code ?? highestLower;
+                        int count;
+                        for (count = 1; count < qty - lastProcessed; count++) 
+                            if ((output[lastProcessed + count].Lower?.Code ?? highestLower) != common) break;
+
+                        // This section obtains those chips and organizes them
+                        ChipCode complement = common.GetFuseComplement(code);
+                        List<VirtualChip> uppers = AcquireHighest(ref set, complement, count);
+                        uppers.Sort((x, y) => x.Code > y.Code);
+                        for (int i = lastProcessed; i < lastProcessed + count; i++) 
+                            output[lastProcessed + i].Upper = uppers[i];
+
+                        // Mark how many chips have been finished
+                        lastProcessed += count;
+                    }
+                }
+                else
+                {
+                    // Obtain as many below-average chips as possible without making fusions
+                    ChipCode highestLower = code.GetDefuseLower();
+                    List<VirtualChip> children = AcquireLowest(ref set, new ChipCode(highestLower.Type, highestLower.Level, highestLower.Weight - 1), qty, false);
+                    int lowers = children.Count;
+
+                    // Grab as many strictly-average chips as we need
+                    if (lowers < qty)
+                        children.AddRange(AcquireExact(ref set, highestLower, 2 * (qty - lowers)));
+
+                    // Since all the below-average chips can be of different levels, we
+                    //  group together identical ones and aim for their true complement
+                    int lastProcessed = 0;
+                    while (lastProcessed < lowers)
+                    {
+                        // This section gets a count of identical chips
+                        ChipCode common = children[lastProcessed].Code;
+                        int count;
+                        for (count = 1; count < lowers - lastProcessed; count++)
+                            if (output[lastProcessed + count].Code != common) break;
+
+                        // This section obtains those chips and organizes them
+                        ChipCode complement = common.GetFuseComplement(code);
+                        List<VirtualChip> uppers = AcquireHighest(ref set, complement, count);
+                        uppers.Sort((x, y) => x.Code > y.Code);
+                        children.AddRange(uppers);
+
+                        // Mark how many chips have been finished
+                        lastProcessed += count;
+                    }
+
+                    // Move all the chips we've collected into fusions
+                    for (int i = 0; i < qty; i++)
+                    {
+                        output[i].Lower = children[i];
+                        output[i].Upper = children[2 * qty - i - 1];
+                    }
+                }
+
+                // Return our results!
+                return output;
+            }
+
+            /// <summary>
+            /// (Of a VirtualChip) Removes empty sub-chips and filles a list with ready-to-fuse chips
+            /// </summary>
+            /// <param name="fusions">List to output fusions in to</param>
+            public void PruneAndReport(ref List<VirtualChip> fusions)
+            {
+                // No reports or pruning on actual chips
+                if (!IsFusion) return;
+
+                // Handle the children
+                if (Lower?.IsEmpty ?? true) Lower = null;
+                else Lower.PruneAndReport(ref fusions);
+
+                if (Upper?.IsEmpty ?? true) Upper = null;
+                else Upper.PruneAndReport(ref fusions);
+
+                // Report!
+                if (CanFuse) fusions.Add(this);
             }
         }
 
@@ -359,7 +498,7 @@ namespace NieR.Automata.Toolkit
             new ChipCode(0x22, 8, 21), // Name = "Heal Drops Up"
         };
 
-        public List<ChipFusion> Fusions = new List<ChipFusion>();
+        public List<VirtualChip> Fusions = new List<VirtualChip>();
         public List<Chip> SellChips = new List<Chip>();
 
         public ChipOptimizer() { }
@@ -369,19 +508,21 @@ namespace NieR.Automata.Toolkit
             // Create our chipset from the input chips
             ChipSet chipSet = new ChipSet();
             foreach (Chip chip in chips) 
-                if (chip.Type != Chip.Empty.Type) chipSet.Add(new VirtualChip(chip));
+                if (chip.Type != Chip.Empty.Type && chip.HasLevels) chipSet.Add(new VirtualChip(chip));
 
             // Create virtual chips and fusions
             foreach (ChipCode target in DesiredChips)
             {
-                VirtualChip chip = VirtualChip.AcquireLowest(target, ref chipSet);
-                //chip?.GreedyPass(ref chipSet, ref Fusions);
+                // Since we request 1 and allow fusions, we're guaranteed 1 chip
+                VirtualChip chip = VirtualChip.AcquireLowest(ref chipSet, target)[0];
+                chip.PruneAndReport(ref Fusions);
             }
 
             // Any unused chips after the greedy pass are sold
             foreach (VirtualChip chip in chipSet)
-                if (!chip.IsFusion()) if (chip.Actual.HasLevels) SellChips.Add(chip.Actual);
+                if (!chip.IsFusion) SellChips.Add(chip.Actual);
 
+            // This sort makes it easier to read through
             Fusions.Sort((x, y) => x.Code > y.Code);
         }
 
